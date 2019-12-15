43ceb3c9b92c57f5f8fc1697bffe7cdb11f08b9e
Maika Kailani <queenmab051@gmail.com>
2019-12-15 18:54:26 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,4 @@
 class ChattyStudent < Student
   def hello
     super 
-    puts "How are you doing today? I'm ok, but I'm kind of tired. Did you watch the Walking Dead last night? You didn't!? "
+    puts "How are you doing today? I'm ok, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! "
