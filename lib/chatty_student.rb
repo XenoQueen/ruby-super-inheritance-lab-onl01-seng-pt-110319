97e9ae1d9cd6d7f550809892c547290fc7b5da5a
Maika Kailani <queenmab051@gmail.com>
2019-12-15 18:55:26 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,4 +4,5 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm ok, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spolers? Okay well let me just tell you who died..."
   end
   
-  
+  def raise_hand
+    
